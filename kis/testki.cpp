317b400a357e7c6296637d8b2d2ac2c3337c09d8
bastinat0r <bastinat0r@bastinat0r.de>
2011-07-06 16:12:22 +0200

    debug message added

--- kis/testki.cpp
+++ kis/testki.cpp
@@ -1,6 +1,8 @@
 #include "testki.h"
+#include <iostream>
 
 PlayerAction TestKI::DoThink(WorldMapView const &vvView, WorldEntityInformation const uEntityInformation)
 {
+    std::cout<<"[testki] DoThink called."<<std::endl;
     return DoNothing;
 }
