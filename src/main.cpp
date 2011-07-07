c2cd1e535889dbe0bfc81f0e7f1b122d94364a90
bastinat0r <bastinat0r@bastinat0r.de>
2011-07-07 11:32:08 +0200

    "Funktioniert jetzt auch mit KIs"

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,8 @@ int main(int argc, char **argv)
 	std::cout<<"[main] samil gestartet"<<std::endl;
 	std::cout<<"[main] zum beenden ESC drücken"<<std::endl;
         Game *pGame = new Game(128,128);
-//        pGame->LoadKI();
-//        pGame->LoadKI();
+        pGame->LoadKI();
+        pGame->LoadKI();
         
         Gui *pGui = new Gui(argc, argv, pGame);
         
